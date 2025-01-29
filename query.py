d7d3686c7bc15cc88e60db8d0b1de50ccbb19697
Davanum Srinivas <davanum@gmail.com>
2025-01-29 17:13:56 -0500

    something worked!
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

---
+++ query.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import openai
+client = openai.Client(
+    base_url="http://127.0.0.1:8000/v1", api_key="EMPTY")
+
+# Chat completion
+response = client.chat.completions.create(
+    model="default",
+    messages=[
+        {"role": "system", "content": "You are a helpful AI assistant"},
+        {"role": "user", "content": "List 3 countries and their capitals."},
+    ],
+    temperature=0,
+    max_tokens=64,
+)
+print(response)
