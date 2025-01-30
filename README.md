59b091c92a3030cba9cbb935b6955ae35a9aeb2b
Davanum Srinivas <davanum@gmail.com>
2025-01-30 17:27:23 -0500

    DeepSeek-R1-Distill-Llama-8B worked!
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ eksdemo create cluster testing-deep-seek-llama-8B-2 -v "1.32" --instance p4d.24x
 
 eksdemo delete cluster testing-deep-seek-llama-8B-2
 
+kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.17.0/deployments/static/nvidia-device-plugin.yml
+
 kubectl port-forward svc/deepseek-r1-server 8000:8000
--- query.py
+++ query.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 import openai
+
 client = openai.Client(
     base_url="http://127.0.0.1:8000/v1", api_key="EMPTY")
 
 # Chat completion
 response = client.chat.completions.create(
-    model="default",
+    model="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
     messages=[
-        {"role": "system", "content": "You are a helpful AI assistant"},
-        {"role": "user", "content": "List 3 countries and their capitals."},
-    ],
-    temperature=0,
-    max_tokens=64,
+        {
+            "role": "user",
+            "content": "List 3 countries and their capitals."
+        },
+    ]
 )
 print(response)
