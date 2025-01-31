63448eb99fc44c31771d7931e708b939de20adbe
Davanum Srinivas <davanum@gmail.com>
2025-01-31 18:29:46 -0500

    totally simplify the yaml
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import openai
+import pprint
 
 client = openai.Client(
     base_url="http://127.0.0.1:8000/v1", api_key="EMPTY")
@@ -11,7 +12,7 @@ response = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "List 3 countries and their capitals."
+            "content": "What is Kubernetes?"
         },
     ]
 )
